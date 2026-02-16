db92c27a8857b2d993b6f3f04eceff04e1fb0861
Nicholas Scott <nicholas.nitrogen@gmail.com>
2026-02-17 00:14:35 +0100

    Break project into modules

--- CountryPathDemo-spi/src/main/java/com/nscott/countrypathdemo/constant/ErrorConstants.java
+++ CountryPathDemo-spi/src/main/java/com/nscott/countrypathdemo/constant/ErrorConstants.java
@@ -3,5 +3,5 @@ package com.nscott.countrypathdemo.constant;
 public class ErrorConstants {
 
     public static final String INVALID_COUNTRY = "Cannot find country with code %s.  Code must be in 3-alpha format.";
-    public static final String NO_POSSIBLE_PATH = "No possible land path between given start and destination";
+    public static final String NO_POSSIBLE_PATH = "No possible land path between given start and destination.";
 }
